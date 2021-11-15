e9ae86f4211aa6cdd3b390fcda4d7cd85b43e69c
Laura Court <laura.mcourt@gmail.com>
2021-11-15 16:29:03 -0500

    Adjust params

--- spec/facades/activity_facade.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe ActivityFacade, :vcr do
-  it 'gets activity info' do
-    activity = ActivityFacade.activity_by_type("recreational", "chicago,il")
-    expect(activity).to be_a(Activity)
-  end
-end
