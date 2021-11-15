48280f8daa2040c12ca1843a0694ad90f08c3ff5
Laura Court <laura.mcourt@gmail.com>
2021-11-15 18:02:26 -0500

    Add [:results].first to service method

--- app/services/background_service.rb
+++ app/services/background_service.rb
@@ -1,7 +1,7 @@
 class BackgroundService
   class << self
     def photo_by_location(location)
-      BackgroundClient.get_data("/search/photos?query=#{location}")
+      BackgroundClient.get_data("/search/photos?query=#{location}")[:results].first
     end
   end
 end
