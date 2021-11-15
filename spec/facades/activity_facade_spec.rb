41170008c7a6199dc3b22ba1161c4066e76542ea
Laura Court <laura.mcourt@gmail.com>
2021-11-15 16:29:16 -0500

    Adjust params to only include location

--- spec/facades/activity_facade_spec.rb
+++ spec/facades/activity_facade_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe ActivityFacade, :vcr do
   it 'gets activity info' do
-    activity = ActivityFacade.activity_by_type("recreational", "chicago,il")
+    activity = ActivityFacade.activity_by_type("chicago,il")
     expect(activity).to be_a(Activity)
   end
 end
