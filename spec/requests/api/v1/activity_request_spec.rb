c3fe619300cb2a6478278fe49a131f92b27461e8
Laura Court <laura.mcourt@gmail.com>
2021-11-15 13:58:10 -0500

    Add test for request

--- spec/requests/api/v1/activity_request_spec.rb
+++ spec/requests/api/v1/activity_request_spec.rb
@@ -4,5 +4,16 @@ describe 'activity API' do
   it 'gets two activities for all temperatures' do
     get '/api/v1/activities?destination=chicago,il'
     expect(response).to be_successful
+
+    activities = JSON.parse(response.body, symbolize_names:true)
+
+    expect(activities[:data]).to be_a(Hash)
+    expect(activities[:data][:id]).to eq(nil)
+    expect(activities[:data][:type]).to eq('activities')
+    expect(activities[:data][:attributes]).to be_a(Hash)
+    expect(activitie[:data][:attributes][:destination]).to be_a(String)
+    expect(activitie[:data][:attributes][:forecast]).to be_a(Hash)
+    expect(activitie[:data][:attributes][:activities]).to be_an(Array)
+
   end
 end
