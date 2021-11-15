0c7d9b85b45503455d9cb31f9e8053df79b1520a
Laura Court <laura.mcourt@gmail.com>
2021-11-15 12:38:38 -0500

    Add location to facade test

--- spec/facades/activity_facade.rb
+++ spec/facades/activity_facade.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe ActivityFacade, :vcr do
   it 'gets activity info' do
-    activity = ActivityFacade("recreational")
+    activity = ActivityFacade.activity_by_type("recreational", "chicago,il")
     expect(activity).to be_a(Activity)
   end
 end
