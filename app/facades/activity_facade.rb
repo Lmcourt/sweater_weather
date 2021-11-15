d85b1875f18f793d33e056ea65ad176a9b41a5eb
Laura Court <laura.mcourt@gmail.com>
2021-11-15 16:28:25 -0500

    Add weather conditional and update variable methods

--- app/facades/activity_facade.rb
+++ app/facades/activity_facade.rb
@@ -1,9 +1,22 @@
 class ActivityFacade
   class << self
-    def activity_by_type(type, location)
-      activity = ActivityService.activity_by_type(type)
-      weather = WeatherFacade.weather_by_location(location)
-      Activity.new(location, activity, weather)
+    def activity_by_type(location)
+      weather_info = WeatherFacade.weather_by_location(location)
+      weather = { summary: weather_info.current_weather[:condtions], temperature: weather_info.current_weather[:temp]}
+      # require "pry"; binding.pry
+      activities = []
+
+      activities << ActivityService.activity_by_type('relaxation')
+
+      if weather[:temperature] >= 60
+        activities << ActivityService.activity_by_type('recreational')
+
+      elsif weather[:temperature] >= 50 && weather[:temperature] < 60
+        activities << ActivityService.activity_by_type('busywork')
+      else
+        activities << ActivityService.activity_by_type('cooking')
+      end
+      Activity.new(destination: location, activities: activities, forecast: weather)
     end
   end
 end
