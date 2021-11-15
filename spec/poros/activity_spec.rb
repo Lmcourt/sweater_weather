370a72850d60a2669e0428aceb7c0f389874a6d0
Laura Court <laura.mcourt@gmail.com>
2021-11-15 16:29:31 -0500

    Add second activity

--- spec/poros/activity_spec.rb
+++ spec/poros/activity_spec.rb
@@ -4,21 +4,32 @@ RSpec.describe Activity, :vcr do
   let(:destination) { {destination: 'chicago,il'} }
   let(:forecast) { {
     summary: 'Cloudy with a chance of meatballs',
-    temperature: '45 F'
+    temperature: 45.12
   } }
-  let(:activities) { { title: 'Learn a new recipe',
+  let(:activities) { [{ title: 'Learn a new recipe',
     type: 'cooking',
     participants: 1,
-    price: 0 }
+    price: 0 },
+    { title: 'chocolate',
+      type: 'recreational',
+      participants: 1,
+      price: 0 }]
   }
+  let(:data) do
+    {
+      destination: destination,
+      forecast: forecast,
+      activities: activities
+    }
+  end
   it 'has attributes' do
-    activity = Activity.new(destination, activities, forecast)
+    activity = Activity.new(data)
         # require "pry"; binding.pry
     expect(activity.destination[:destination]).to eq("chicago,il")
     expect(activity.forecast).to eq({
       summary: 'Cloudy with a chance of meatballs',
-      temperature: '45 F'
+      temperature: 45.12
     })
-    expect(activities[:type]).to eq('cooking')
+    expect(activities.first[:type]).to eq('cooking')
   end
 end
