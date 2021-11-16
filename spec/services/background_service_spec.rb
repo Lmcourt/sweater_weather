001c30ec4f8d7ffed1a33ee3506aa8c180c79374
Laura Court <laura.mcourt@gmail.com>
2021-11-15 19:05:06 -0500

    Adjust key hash values

--- spec/services/background_service_spec.rb
+++ spec/services/background_service_spec.rb
@@ -4,20 +4,18 @@ RSpec.describe BackgroundService, :vcr do
   it 'returns a background images data' do
     photo = BackgroundService.photo_by_location('chicago')
     expect(photo).to be_a(Hash)
-    expect(photo.size).to eq(3)
+    expect(photo.size).to eq(20)
+    expect(photo).to have_key(:urls)
+    expect(photo[:urls]).to be_a(Hash)
 
-    result = photo[:results].first
-    expect(result).to have_key(:urls)
-    expect(result[:urls]).to be_a(Hash)
+    expect(photo[:urls]).to have_key(:raw)
+    expect(photo[:urls][:raw]).to be_a(String)
 
-    expect(result[:urls]).to have_key(:raw)
-    expect(result[:urls][:raw]).to be_a(String)
+    expect(photo[:user]).to be_a(Hash)
+    expect(photo[:user]).to have_key(:name)
+    expect(photo[:user][:name]).to be_a(String)
 
-    expect(result[:user]).to be_a(Hash)
-    expect(result[:user]).to have_key(:name)
-    expect(result[:user][:name]).to be_a(String)
-
-    expect(result[:user][:links]).to have_key(:html)
-    expect(result[:user][:links][:html]).to be_a(String)
+    expect(photo[:user][:links]).to have_key(:html)
+    expect(photo[:user][:links][:html]).to be_a(String)
   end
 end
