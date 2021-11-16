75456a16e2a9c6e829e5a13cd8d62a34c3ad5c86
Laura Court <laura.mcourt@gmail.com>
2021-11-15 19:03:26 -0500

    Add location to poro params

--- app/facades/background_facade.rb
+++ app/facades/background_facade.rb
@@ -2,7 +2,7 @@ class BackgroundFacade
   class << self
     def photo_by_location(location)
       photo_info = BackgroundService.photo_by_location(location)
-      Background.new(photo_info)
+      Background.new(photo_info, location)
     end
   end
 end
