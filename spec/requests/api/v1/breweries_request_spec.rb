7b56849d42888d057bc6f34d8e2bfc5bcb654c59
Laura Court <laura.mcourt@gmail.com>
2021-11-16 12:13:39 -0500

    Add expectations to breweries test

--- spec/requests/api/v1/breweries_request_spec.rb
+++ spec/requests/api/v1/breweries_request_spec.rb
@@ -7,8 +7,26 @@ describe 'breweries API', :vcr do
     expect(response).to be_successful
     brew = JSON.parse(response.body, symbolize_names: true)
 
+    expect(brew).to have_key(:data)
     expect(brew[:data]).to be_a(Hash)
+
+    expect(brew[:data]).to have_key(:id)
+    expect(brew[:data]).to have_key(:type)
+    expect(brew[:data]).to have_key(:attributes)
     expect(brew[:data][:id]).to eq(nil)
     expect(brew[:data][:type]).to eq('breweries')
+    expect(brew[:data][:attributes]).to be_a(Hash)
+
+    expect(brew[:data][:attributes]).to have_key(:destination)
+    expect(brew[:data][:attributes]).to have_key(:forecast)
+    expect(brew[:data][:attributes]).to have_key(:breweries)
+    expect(brew[:data][:attributes][:destination]).to be_a(String)
+    expect(brew[:data][:attributes][:forecast]).to be_a(Hash)
+
+    expect(brew[:data][:attributes][:breweries]).to be_an(Array)
+    brew_attr= brew[:data][:attributes][:breweries].first
+    expect(brew_attr[:id]).to be_a(String)
+    expect(brew_attr[:name]).to be_a(String)
+    expect(brew_attr[:brewery_type]).to be_a(String)
   end
 end
