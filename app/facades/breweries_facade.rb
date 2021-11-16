edf2151078e2e231371173b50464be1b50f936b8
Laura Court <laura.mcourt@gmail.com>
2021-11-16 12:45:45 -0500

    Create new methods for weather and coordinates

--- app/facades/breweries_facade.rb
+++ app/facades/breweries_facade.rb
@@ -1,11 +1,17 @@
 class BreweriesFacade
   class << self
     def breweries_by_location(location, quantity)
-      coords = WeatherFacade.coordinates(location)
-      weather_info = WeatherFacade.weather_by_location(location)
-      weather = { summary: weather_info.current_weather[:conditions], temperature: weather_info.current_weather[:temp]}
-      breweries = BreweriesService.breweries_by_location(coords, quantity)
-      Breweries.new(destination: location, breweries: breweries, forecast: weather)
+      breweries = BreweriesService.breweries_by_location(coordinates(location), quantity)
+      Breweries.new(destination: location, breweries: breweries, forecast: weather_info(location))
+    end
+
+    def coordinates(location)
+      WeatherFacade.coordinates(location)
+    end
+
+    def weather_info(location)
+      weather = WeatherFacade.weather_by_location(location)
+      { summary: weather.current_weather[:conditions], temperature: weather.current_weather[:temp]}
     end
   end
 end
