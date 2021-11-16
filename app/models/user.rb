0022bff9521bc5640a8ce9901a038e10cdb749b2
Laura Court <laura.mcourt@gmail.com>
2021-11-15 21:43:21 -0500

    Add email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :username, uniqueness: true
+  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
