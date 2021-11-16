5d62b1230151ac4be41a3e0522f45a7dac1042fb
Laura Court <laura.mcourt@gmail.com>
2021-11-16 12:31:05 -0500

    Add coordinates to location

--- app/facades/breweries_facade.rb
+++ app/facades/breweries_facade.rb
@@ -1,9 +1,10 @@
 class BreweriesFacade
   class << self
     def breweries_by_location(location, quantity)
+      coords = WeatherFacade.coordinates(location)
       weather_info = WeatherFacade.weather_by_location(location)
       weather = { summary: weather_info.current_weather[:conditions], temperature: weather_info.current_weather[:temp]}
-      breweries = BreweriesService.breweries_by_location(location, quantity)
+      breweries = BreweriesService.breweries_by_location(coords, quantity)
       Breweries.new(destination: location, breweries: breweries, forecast: weather)
     end
   end
