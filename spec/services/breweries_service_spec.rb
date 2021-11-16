2148c4a6dae7a88dec95c5e338c34dc9dd677799
Laura Court <laura.mcourt@gmail.com>
2021-11-16 12:31:27 -0500

    Adjust test to have coordinates

--- spec/services/breweries_service_spec.rb
+++ spec/services/breweries_service_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe BreweriesService, :vcr do
   it 'returns breweries' do
-    brew = BreweriesService.breweries_by_location("38.8977,77.0365", 5)
+    brew = BreweriesService.breweries_by_location({ lat: 40.039401, lng: -76.307078 }, 5)
 
     expect(brew).to be_an(Array)
   end
