db0bb20c54b7821a6aebb4eebba190ef24b75996
Laura Court <laura.mcourt@gmail.com>
2021-11-15 22:04:06 -0500

    Change expect activite to activities

--- spec/requests/api/v1/activity_request_spec.rb
+++ spec/requests/api/v1/activity_request_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'activity API' do
+describe 'activity API', :vcr do
   it 'gets two activities for all temperatures' do
     get '/api/v1/activities?destination=chicago,il'
     expect(response).to be_successful
@@ -9,11 +9,11 @@ describe 'activity API' do
 
     expect(activities[:data]).to be_a(Hash)
     expect(activities[:data][:id]).to eq(nil)
-    expect(activities[:data][:type]).to eq('activities')
+    expect(activities[:data][:type]).to eq('activity')
     expect(activities[:data][:attributes]).to be_a(Hash)
-    expect(activitie[:data][:attributes][:destination]).to be_a(String)
-    expect(activitie[:data][:attributes][:forecast]).to be_a(Hash)
-    expect(activitie[:data][:attributes][:activities]).to be_an(Array)
+    expect(activities[:data][:attributes][:destination]).to be_a(String)
+    expect(activities[:data][:attributes][:forecast]).to be_a(Hash)
+    expect(activities[:data][:attributes][:activities]).to be_an(Array)
 
   end
 end
