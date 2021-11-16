de5153ecbdd9073f5550fdfc737c2bdbf37088ac
Laura Court <laura.mcourt@gmail.com>
2021-11-16 13:24:00 -0500

    Test for invalid quantity

--- spec/requests/api/v1/breweries_request_spec.rb
+++ spec/requests/api/v1/breweries_request_spec.rb
@@ -29,4 +29,13 @@ describe 'breweries API', :vcr do
     expect(brew_attr[:name]).to be_a(String)
     expect(brew_attr[:brewery_type]).to be_a(String)
   end
+
+  it 'defaults to 20 breweries if not given a valid quantity' do
+    get '/api/v1/breweries?location=denver,co&quantity=-1'
+
+    expect(response).to be_successful
+    brew = JSON.parse(response.body, symbolize_names: true)
+
+    expect(brew[:data][:attributes][:breweries].count).to eq(20)
+  end
 end
