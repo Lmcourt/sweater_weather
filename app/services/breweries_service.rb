ccf30e50814d28b2e96ae926091ed5a87335cfb8
Laura Court <laura.mcourt@gmail.com>
2021-11-16 11:17:14 -0500

    Add breweries service spec

---
+++ app/services/breweries_service.rb
@@ -0,0 +1,8 @@
+class BreweriesService
+  class << self
+    def breweries_by_location(location, quantity)
+      BreweriesClient.get_data("/breweries?by_dist=#{location}&per_page=#{quantity}")
+      require "pry"; binding.pry
+    end
+  end
+end
