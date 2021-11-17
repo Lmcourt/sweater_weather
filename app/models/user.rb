d6f399e7928a5e7995c1046e471c6773124c19c6
Laura Court <laura.mcourt@gmail.com>
2021-11-16 20:29:06 -0500

    Add password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   has_secure_token :api_key
 
   validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true
+  validates :password_confirmation, presence: { on: :create }
 end
