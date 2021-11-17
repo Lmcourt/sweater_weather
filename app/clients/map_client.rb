6e6cef0990e19e555946262929fa79f02f0950b5
Laura Court <laura.mcourt@gmail.com>
2021-11-17 11:42:13 -0500

    Add headers and params

--- app/clients/map_client.rb
+++ app/clients/map_client.rb
@@ -4,6 +4,14 @@ class MapClient
       parse_data(conn.get(url))
     end
 
+    def get_directions(url, data)
+      response = conn.get(url) do |r|
+        r.params['from'] = data[:origin]
+        r.params['to'] = data[:destination]
+      end
+      parse_data(response)
+    end
+
     private
 
     def parse_data(response)
@@ -13,7 +21,8 @@ class MapClient
     def conn
       Faraday.new(
         url: "http://www.mapquestapi.com",
-        params: { key: ENV['map_api'] }
+        params: { key: ENV['map_api'] },
+        headers: { 'Content-Type' => 'application/json' }
       )
     end
   end
