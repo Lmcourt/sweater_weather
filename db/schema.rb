10a769877fab0a23e285c7ecc82de1a07ed801f0
Laura Court <laura.mcourt@gmail.com>
2021-11-16 19:10:38 -0500

    Add api_key to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_16_004952) do
+ActiveRecord::Schema.define(version: 2021_11_16_221507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "api_keys", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_api_keys_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "api_key"
+    t.index ["api_key"], name: "index_users_on_api_key", unique: true
   end
 
+  add_foreign_key "api_keys", "users"
 end
